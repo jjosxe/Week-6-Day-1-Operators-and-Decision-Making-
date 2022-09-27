5c10f3017874fa84280d06fab136a72feae80715
Jose Jimenez <113552145+jjosxe@users.noreply.github.com>
2022-09-27 19:42:33 +0000

    Append and remove

--- main.py
+++ main.py
@@ -15,14 +15,18 @@
 seconds = [1.23, 1.45, 1.02]
 current = 1.11
 
+seconds.append(current)
+print(seconds)
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
 
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
-
+seconds.remove(1.45)
+seconds.remove(1.02)
+newseconds = seconds[1:]
+print(newseconds)
 ################################comparison operators#########################
 #remember....
 # > greater
